1b65180866ce821786e1754e45143bebd31aee7a
first-name second-name <silvia.wachira@student.moringa.com>
2022-11-01 09:23:39 +0300

    completed assignment

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,25 @@
 class Triangle
-  # write code here
+
+  def initialize(s1, s2, s3)
+    @s1 = s1
+    @s2 = s2
+    @s3 = s3
+  end
+
+  def kind
+    all_sides = [@s1, @s2, @s3].sort
+    if all_sides.any?{|side| side <= 0} || (all_sides[0] + all_sides[1] <= all_sides[2])
+      raise TriangleError
+    elsif @s1 == @s2 && @s2 == @s3
+      :equilateral
+    elsif @s1 == @s2 || @s2 == @s3 || @s1 == @s3
+      :isosceles
+    else
+      :scalene
+    end
+
+  end
+
+  class TriangleError < StandardError
+  end
 end
